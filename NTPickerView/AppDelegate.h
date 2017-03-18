1d3ce3fef63ff9dd782708bc53ba85731ade652e
NtZheng <2745674085@qq.com>
2017-03-18 11:49:03 +0800

    Initial Commit

---
+++ NTPickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NTPickerView
+//
+//  Created by Nineteen on 3/18/17.
+//  Copyright © 2017 Nineteen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NTPickerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NTPickerView
+//
+//  Created by Nineteen on 3/18/17.
+//  Copyright © 2017 Nineteen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
